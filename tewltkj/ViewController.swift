37c804f7c3c643dcfcae515bbeeeafed19fe6411
Mikola Marchuk <m.m.marchuk@gmail.com>
2020-08-04 16:03:58 +0300

    Initial Commit

---
+++ tewltkj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tewltkj
+//
+//  Created by Nick on 04.08.2020.
+//  Copyright © 2020 Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
